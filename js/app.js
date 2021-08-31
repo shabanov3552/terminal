e3fcf9e52700d23db4a644cd04ca659bb9b3ff14
aleksandr <60693889+shabanov3552@users.noreply.github.com>
2021-08-31 23:03:09 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -400,7 +400,7 @@ window.addEventListener("load", function () {
 	if (document.querySelector('.wrapper')) {
 		setTimeout(function () {
 			document.querySelector('.wrapper').classList.add('_loaded');
-		}, 0);
+		}, 100);
 	}
 });
 
